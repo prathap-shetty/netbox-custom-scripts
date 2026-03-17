062b50e5d58838a37175ee5da24e733e1a2c07e8
Prathap Shetty <shettypratap75@gmail.com>
2026-03-17 14:25:41 +0000

    Update add_network_device.py

--- scripts/add_network_device.py
+++ scripts/add_network_device.py
@@ -326,7 +326,7 @@ class CommissionDevice(Script):
 
 
     def _allocate_next_child_31(self, parent):
-        token = os.getenv("NETBOX_API_TOKEN")
+        token = os.getenv("NETBOX_TOKEN")
         if not token:
             raise AbortScript("Missing NETBOX_API_TOKEN environment variable")
     
